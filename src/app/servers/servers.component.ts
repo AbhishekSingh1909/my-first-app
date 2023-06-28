9a92f34f70f3bd503ce6b2457f67463ac323b622
Abhishek Singh <singh90abhishek@gmail.com>
2023-06-29 00:45:42 +0300

    commit selector approach

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // as element
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  //as attribute
+  //selector : `[app-servers]`,
+  // as class
+ // selector : '.app-servers' ,
+  template: `<app-server>
+
+  </app-server>
+  <h1>I M using template under servers component.ts</h1>
+  <app-server></app-server>`,
+  styles: [`h1{color: red}`]
 })
 export class ServersComponent {
 
