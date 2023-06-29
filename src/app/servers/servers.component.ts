6bb8c65e4b025e5f23d6a4f11fa3a329c21eecb9
Abhishek Singh <singh90abhishek@gmail.com>
2023-06-29 16:42:53 +0300

    all form data binding like two way data binding by ngModel

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,13 +7,33 @@ import { Component } from '@angular/core';
   //selector : `[app-servers]`,
   // as class
  // selector : '.app-servers' ,
-  template: `<app-server>
+ // template example
+  // template: `<app-server>
 
-  </app-server>
-  <h1>I M using template under servers component.ts</h1>
-  <app-server></app-server>`,
+  // </app-server>
+  // <h1>I M using template under servers component.ts</h1>
+  // <app-server></app-server>`,
+
+  // tempalatURL
+  templateUrl : './servers.component.html',
   styles: [`h1{color: red}`]
 })
 export class ServersComponent {
+allowserver : boolean = false;
+serverCreationStaus : string = 'No Server is created';
+serverName = 'testServer';
 
+constructor() {
+  setTimeout(()=>{this.allowserver = true;},2000)
+}
+onServerCreation() : string
+{
+  return this.serverCreationStaus ='Server is created and Name of server is '+ this.serverName;
+}
+onUpdateServerName(event: any)
+{
+  console.log(event);
+  this.serverName = (<HTMLInputElement>event.target).value;
+
+}
 }
